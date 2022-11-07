9c669acce7013f3fb8d6ce9f7b6147449259c226
Евгений Шевченко <3530385@gmail.com>
2022-11-08 02:24:25 +0300

    some fix

--- model/knn_fit.py
+++ model/knn_fit.py
@@ -1,17 +1,17 @@
 from sklearn.neighbors import KNeighborsRegressor
 import pandas as pd
 
-def get_neib():
+def get_neib(k_neib = 10):
     df_cian = pd.read_csv('database/worker_data/data.csv')
     df_input = pd.read_csv('database/worker_data/input_data.csv')
     df_cian = df_cian[df_cian.notna()]
     df_cian = df_cian.fillna(method="pad")
     X = df_cian[["lat", "lng", "area"]]
     y = df_cian["cost"]
-    neigh = KNeighborsRegressor(n_neighbors=10)
+    neigh = KNeighborsRegressor(n_neighbors=k_neib)
     neigh.fit(X, y)
     k_n = neigh.kneighbors(df_input)
-    return df_cian, k_n
+    return df_cian[:len(k_n)], k_n
 
 
 def main():
